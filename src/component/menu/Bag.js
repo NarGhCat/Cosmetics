f7d58779e6a459f0d56a23064e0eefa601753546
Kristine <kristine.gh99@gmail.com>
2021-04-09 19:03:39 +0400

    bag changes

--- src/component/menu/Bag.js
+++ src/component/menu/Bag.js
@@ -2,6 +2,12 @@ import React, { useEffect, useState } from "react";
 import "../../styles/bag.css";
 
 const Bag = () => {
+  const [displayNone, setDisplay] = useState(false);
+
+  const handleAfterpay = (e) => {
+    setDisplay(displayNone ? false : true);
+  };
+
   return (
     <div className="bag-component">
       <div className="bag-component-heading">
@@ -12,9 +18,23 @@ const Bag = () => {
       </span>
       <span className="bag-component-right">
         <div className="bag-component-right-items">
-            <h4>ORDER SUMMARY</h4>
-            <p className="bag-component-right-text">SUBTOTAL</p>
-            <p className="bag-component-right-text">SHIPPING FREE</p>
+          <h4>ORDER SUMMARY</h4>
+          <p className="bag-component-right-text">SUBTOTAL {}</p>
+          <p className="bag-component-right-text shipping">SHIPPING </p>
+          <p className="shipping-free">FREE</p>
+          <div className="shipping-text">
+            <p>STANDARD (3-5 BUS DAYS)</p>
+          </div>
+          <p className="bag-component-right-text shipping">ESTIMATED TOTAL</p>
+          <p className="shipping-free price-total">$ 0</p>
+          <div className="popup-for-afterpay">
+            <p className="popup-for-afterpay-text">
+              Pay in 4 installments on orders $35 - $1000 by{" "}
+            </p>
+            <span onClick={handleAfterpay} className="afterpay-info">
+               {(img ? img : "i")}
+            </span>
+          </div>
         </div>
       </span>
     </div>
