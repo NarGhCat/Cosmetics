728f80fe17a4c9df55f6f36cb420950e9ffccae7
Siirusho <74008883+Siirusho@users.noreply.github.com>
2021-04-17 18:53:59 +0400

    fixing add to bag and learn more buttons in New component

--- src/component/section/New.js
+++ src/component/section/New.js
@@ -1,39 +1,44 @@
 import "../../styles/NewStyle.css";
 import React, { useState, useEffect } from "react";
-import { selectNews } from "../../selectors/fierbase";
-import { useSelector } from "react-redux";
-import { storage } from "../../index";
-import Card from "../shared/Card";
-import Button from "../shared/Button";
-import CardMedia from "../shared/CardMedia";
-import Typography from "../shared/Typography";
+import { selectNews, selectUser } from "../../selectors/fierbase";
+import { useSelector, useDispatch } from "react-redux";
+import { db, storage } from "../../index";
+// import Card from "../shared/Card";
+// import Button from "../shared/Button";
+// import CardMedia from "../shared/CardMedia";
+// import Typography from "../shared/Typography";
+// import { Link, useHistory } from "react-router-dom";
 import {
-  CardActionArea,
-  CardActions,
-  CardContent,
-  makeStyles,
+  // CardActionArea,
+  // CardActions,
+  // CardContent,
+  makeStyles
 } from "@material-ui/core";
-import { useParams } from "react-router-dom";
+
 import SideBar from "./SideBar";
+import { SET_SELECTED_ITEM, SET_USER } from "../../reducer/reducer";
+import firebase from "firebase/app";
+import { useAlert } from "react-alert";
+import Item from "./Item";
+
 const useStyles = makeStyles({
   brandRoot: {
     display: "flex",
     width: 95 + "%",
     margin: "auto",
-    justifyContent: "space-between",
+    justifyContent: "space-between"
   },
   brandItem: {
     display: "flex",
     flexFlow: "wrap",
     justifyContent: "space-between",
-    width: 82 + "%",
-  },
+    width: 82 + "%"
+  }
 });
 
-const New = () => {
+const New = (props) => {
   const brandClasses = useStyles();
   const news = useSelector(selectNews);
-  const { brandUrl } = useParams();
   const [imgs, setImgs] = useState([]);
 
   async function getImgUrl(path) {
@@ -64,45 +69,56 @@ const New = () => {
     <div className={brandClasses.brandRoot}>
       <SideBar />
       <div className={brandClasses.brandItem}>
-        {news.map((item, i) =>
-          item.brandId.id ? (
-            <Card key={i}>
-              <Typography>{brandUrl}</Typography>
-              <CardActionArea>
-                <CardMedia image={imgs[i]} />
-                <CardContent>
-                  <Typography>{item.name}</Typography>
-                  <Typography>$ {item.price}</Typography>
-                </CardContent>
-              </CardActionArea>
-              <CardActions>
-                <Button
-                  bgColor="white"
-                  labelcolor="#4c003f"
-                  width="140px"
-                  border="none"
-                >
-                  {" "}
-                  Add to Bag
-                </Button>
-
-                <Button
-                  bgColor="white"
-                  labelcolor="#4c003f"
-                  width="140px"
-                  border="none"
-                >
-                  {" "}
-                  Learn More
-                </Button>
-              </CardActions>
-            </Card>
-          ) : (
-            ""
-          )
-        )}
+        {news.map((item, i) => (
+          <Item key={item.itemId} ind={i} {...item} />
+        ))}
       </div>
     </div>
+    // <div className={brandClasses.brandRoot}>
+    //   <SideBar />
+    //   <div className={brandClasses.brandItem}>
+    //     {news.map((item, i) =>
+    //       item.brandId.id ? (
+    //         <Card key={i}>
+    //           <Typography>{brandUrl}</Typography>
+    //           <CardActionArea>
+    //             <CardMedia image={imgs[i]} />
+    //             <CardContent>
+    //               <Typography>{item.name}</Typography>
+    //               <Typography>$ {item.price}</Typography>
+    //             </CardContent>
+    //           </CardActionArea>
+    //           <CardActions>
+    //             <Button
+    //               bgColor="white"
+    //               labelcolor="#4c003f"
+    //               width="140px"
+    //               border="none"
+    //               onClick={() => { handleAddToBagItem({ ...props }, user); }}
+    //             >
+    //               {" "}
+    //               Add to Bag
+    //             </Button>
+    //             <Link to="/clickedItem">
+    //       <Button
+    //         bgColor="white"
+    //         labelcolor="#4c003f"
+    //         width="140px"
+    //         border="none"
+    //         onClick={() => handleLearnMore({ ...props })}
+    //       >
+    //         {" "}
+    //         Learn More
+    //       </Button>
+    //     </Link>
+    //           </CardActions>
+    //         </Card>
+    //       ) : (
+    //         ""
+    //       )
+    //     )}
+    //   </div>
+    // </div>
   );
 };
 export default New;
--- src/selectors/fierbase.js
+++ src/selectors/fierbase.js
@@ -1,9 +1,12 @@
-export const selectBrands = state => state.brands
-export const selectCategories = state => state.categories
-export const selectItems = state => state.items
-export const selectNews = state => state.news
-export const selectCategoryById = (id) => (state) => state.categories.find(category => category.categoryId === id)
-export const selectBrandById = (id) => (state) => state.brands.find(brand => brand.brandId === id)
-export const selectUser = state => state.user
-export const selectedItem = state => state.selectedItem
-export const selectBagCount = state => state.user && state.user.data.bag.length || 0
+export const selectBrands = (state) => state.brands;
+export const selectCategories = (state) => state.categories;
+export const selectItems = (state) => state.items;
+export const selectNews = (state) => state.news;
+export const selectCategoryById = (id) => (state) =>
+  state.categories.find((category) => category.categoryId === id);
+export const selectBrandById = (id) => (state) =>
+  state.brands.find((brand) => brand.brandId === id);
+export const selectUser = (state) => state.user;
+export const selectedItem = (state) => state.selectedItem;
+export const selectBagCount = (state) =>
+  (state.user && state.user.data.bag.length) || 0;
